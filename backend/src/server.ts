ee9c117d6d651cc57a6ffac0acac6e72e83dc475
dojo <djdumaluan.prog@gmail.com>
2026-06-07 14:48:59 +0800

    feat: apply storage conn test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,12 @@ import http from "http";
 import app from "./app";
 import ENV from "./lib/env";
 import testDBConnection from "./database/test";
+import testStorageConnection from "./storage/test";
 
 const startServer = async () => {
   try {
     const dbConnStr = await testDBConnection();
+    const strgConnStr = await testStorageConnection();
 
     const server = http.createServer(app);
     const PORT = ENV.PORT || 3000;
@@ -13,6 +15,7 @@ const startServer = async () => {
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
       console.log(dbConnStr);
+      console.log(strgConnStr);
     });
   } catch (error) {
     console.error(error);
