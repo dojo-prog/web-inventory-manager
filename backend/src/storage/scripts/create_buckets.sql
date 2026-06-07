44a52f2b800e435a89cf45af3ad26d91768b0b65
dojo <djdumaluan.prog@gmail.com>
2026-06-07 21:45:41 +0800

    chore: create storage buckets

---
+++ backend/src/storage/scripts/create_buckets.sql
@@ -0,0 +1,11 @@
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('user-avatars', 'user-avatars', true);
+
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('brand-logos', 'brand-logos', true);
+
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('product-thumbnails', 'product-thumbnails', true);
+
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('product-images', 'product-images', true);
