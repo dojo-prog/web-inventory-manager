14111011a8f36d1008c70731fbd4fddf0162ef29
dojo <djdumaluan.prog@gmail.com>
2026-06-07 14:18:55 +0800

    feat: apply db connection to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,18 @@
 import http from "http";
 import app from "./app";
 import ENV from "./lib/env";
+import testDBConnection from "./database/test";
 
 const startServer = async () => {
   try {
+    const dbConnStr = await testDBConnection();
+
     const server = http.createServer(app);
     const PORT = ENV.PORT || 3000;
 
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
+      console.log(dbConnStr);
     });
   } catch (error) {
     // TODO apply error handler
