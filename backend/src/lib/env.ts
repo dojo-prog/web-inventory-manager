511004941ba375b9b2a473d031960f11c0f36d8f
dojo <djdumaluan.prog@gmail.com>
2026-06-07 14:48:20 +0800

    chore: add supabase proj url & anon key

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -4,6 +4,8 @@ const ENV = {
   PORT: process.env.PORT,
   CLIENT_URL: process.env.CLIENT_URL,
   DATABASE_URL: process.env.DATABASE_URL,
+  SUPABASE_PROJECT_URL: process.env.SUPABASE_PROJECT_URL,
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
 };
 
 export default ENV;
