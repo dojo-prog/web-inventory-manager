bdec8320dc6173e04c8e849317ac887f9bff44ea
dojo <djdumaluan.prog@gmail.com>
2026-06-07 22:37:20 +0800

    chore: add refreshAccessToken endpoint

--- backend/src/modules/auth/auth.route.ts
+++ backend/src/modules/auth/auth.route.ts
@@ -3,12 +3,18 @@ import {
   authorizeRoles,
   protectRoute,
 } from "../../middlewares/auth.middleware";
-import { getCurrentUser, login, logout } from "./auth.controller";
+import {
+  getCurrentUser,
+  login,
+  logout,
+  refreshAccessToken,
+} from "./auth.controller";
 
 const router = express.Router();
 
 router.get("/me", protectRoute, getCurrentUser);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/refresh", refreshAccessToken);
 
 export default router;
