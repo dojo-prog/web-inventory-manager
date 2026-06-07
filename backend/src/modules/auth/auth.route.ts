e90cf757044e88bc24057cc27b20becec61f9f92
dojo <djdumaluan.prog@gmail.com>
2026-06-07 22:43:54 +0800

    chore: apply input validation to login

--- backend/src/modules/auth/auth.route.ts
+++ backend/src/modules/auth/auth.route.ts
@@ -9,11 +9,13 @@ import {
   logout,
   refreshAccessToken,
 } from "./auth.controller";
+import { validate } from "../../middlewares/validation.middleware";
+import { LoginInputSchema } from "@web-inventory-manager/shared";
 
 const router = express.Router();
 
 router.get("/me", protectRoute, getCurrentUser);
-router.post("/login", login);
+router.post("/login", validate(LoginInputSchema), login);
 router.post("/logout", logout);
 router.post("/refresh", refreshAccessToken);
 
