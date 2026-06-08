b62b2af1639d98b0fc06476512369f156c415e1a
dojo <djdumaluan.prog@gmail.com>
2026-06-08 22:14:38 +0800

    chore: create build update fields query that returns the setClause and values

---
+++ backend/src/utils/buildUpdateFields.ts
@@ -0,0 +1,20 @@
+interface UpdateFieldsResult {
+  setClause: string;
+  values: any[];
+}
+
+const buildUpdateFields = (
+  changes: Record<string, any>,
+): UpdateFieldsResult => {
+  const changesKeys = Object.keys(changes).filter(
+    (k) => changes[k] !== undefined && changes[k] !== null,
+  );
+
+  const setClause = changesKeys.map((k, i) => `${k} = $${i + 1}`).join(", ");
+  const values = changesKeys.map((k) => changes[k]);
+
+  return {
+    setClause,
+    values,
+  };
+};
