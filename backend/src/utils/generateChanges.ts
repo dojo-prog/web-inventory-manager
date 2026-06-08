9ed40e046c440db962eeb7f61122b350bac2e65a
dojo <djdumaluan.prog@gmail.com>
2026-06-08 22:57:20 +0800

    chore: create util that generates changes by comparing original value from update inputs

---
+++ backend/src/utils/generateChanges.ts
@@ -0,0 +1,16 @@
+const generateChanges = (
+  original: Record<string, any>,
+  modified: Record<string, any>,
+): Record<string, any> => {
+  let changes: Record<string, any> = {};
+
+  const comparisonKeys = Object.keys(modified);
+
+  comparisonKeys.forEach((k) => {
+    if (original[k] !== modified[k]) {
+      changes[k] = modified[k];
+    }
+  });
+
+  return changes;
+};
