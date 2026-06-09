9a73be4a19a908fd6cf51c574ee4325f32bf0c7b
dojo <djdumaluan.prog@gmail.com>
2026-06-10 02:00:09 +0800

    refactor: extend validation middleware to handle params & query validation

--- backend/src/middlewares/validation.middleware.ts
+++ backend/src/middlewares/validation.middleware.ts
@@ -1,22 +1,57 @@
-import { ZodTypeAny } from "zod";
 import { Middleware } from "../types/handlers";
 import AppError from "../utils/AppError";
+import { AnyZodObject } from "zod/v3";
 
-export const validate = (schema: ZodTypeAny): Middleware => {
+interface ValidationRequestSchema {
+  params?: AnyZodObject;
+  query?: AnyZodObject;
+  body?: AnyZodObject;
+}
+
+export const validate = (schemas: ValidationRequestSchema): Middleware => {
   return (req, res, next) => {
-    const combinedData = { ...req.body, ...req.file, ...req.files };
+    const validationError: Record<string, any> = {};
 
-    const result = schema.safeParse(combinedData);
+    // Params validation
+    if (schemas.params) {
+      const result = schemas.params.safeParse(req.params);
 
-    if (!result.success) {
-      return next(
-        new AppError("Validation Error", 400, {
-          errors: result.error.flatten().fieldErrors,
-        }),
-      );
+      if (!result.success) {
+        Object.assign(validationError, result.error.flatten().fieldErrors);
+      } else {
+        req.params = result.data;
+      }
     }
 
-    req.body = result.data;
+    // Query Validation
+    if (schemas.query) {
+      const result = schemas.query.safeParse(req.query);
+
+      if (!result.success) {
+        Object.assign(validationError, result.error.flatten().fieldErrors);
+      } else {
+        req.query = result.data;
+      }
+    }
+
+    // Body & Files Validation
+    if (schemas.body) {
+      const combinedData = { ...req.body, ...req.file, ...req.files };
+
+      const result = schemas.body.safeParse(combinedData);
+
+      if (!result.success) {
+        Object.assign(validationError, result.error.flatten().fieldErrors);
+      } else {
+        req.body = result.data;
+      }
+    }
+
+    if (Object.keys(validationError).length > 0) {
+      return next(
+        new AppError("Validation Error", 400, { errors: validationError }),
+      );
+    }
 
     next();
   };
