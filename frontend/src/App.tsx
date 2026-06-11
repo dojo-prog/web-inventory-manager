c85c06a42fdb2cbef2aa7f245538ef545b405aed
dojo <djdumaluan.prog@gmail.com>
2026-06-11 17:19:19 +0800

    chore: render toast container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { Slide, ToastContainer } from "react-toastify";
 import AppRouter from "./app/routers";
 
 const App = () => {
   return (
     <>
       <AppRouter />
+      <ToastContainer autoClose={1500} transition={Slide} />
     </>
   );
 };
