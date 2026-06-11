843ed589c29b0fb0cd8e18351ef2dc363e3e7309
dojo <djdumaluan.prog@gmail.com>
2026-06-11 16:19:16 +0800

    feat: implement auth feature

--- frontend/src/features/auth/auth.service.ts
+++ frontend/src/features/auth/auth.service.ts
@@ -1,6 +1,22 @@
-import type { LoginInput } from "@web-inventory-manager/shared/dist";
+import type { LoginInput, User } from "@web-inventory-manager/shared";
+import axios from "../../lib/axios";
 
-export const checkAuth = async () => {};
-export const login = async (inputs: LoginInput) => {};
-export const logout = async () => {};
-export const refresh = async () => {};
+export const checkAuth = async (): Promise<User> => {
+  const res = await axios.post("/auth/me");
+
+  return res.data.user;
+};
+
+export const login = async (inputs: LoginInput): Promise<User> => {
+  const res = await axios.post("/auth/login", inputs);
+
+  return res.data.user;
+};
+
+export const logout = async (): Promise<void> => {
+  await axios.post("/auth/logout");
+};
+
+export const refresh = async (): Promise<void> => {
+  await axios.post("/auth/refresh");
+};
--- frontend/src/features/auth/auth.store.ts
+++ frontend/src/features/auth/auth.store.ts
@@ -1,5 +1,8 @@
 import { create } from "zustand";
 import type { AuthState } from "./auth.types";
+import errorHandler from "../../utils/errorHandler";
+import * as authService from "./auth.service";
+import { toast } from "react-toastify";
 
 const useAuthStore = create<AuthState>((set) => ({
   user: null,
@@ -7,10 +10,52 @@ const useAuthStore = create<AuthState>((set) => ({
   checkingAuth: false,
   loading: false,
 
-  checkAuth: async () => {},
-  login: async (loginInputs) => {},
-  logout: async () => {},
-  refresh: async () => {},
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+    try {
+      const user = await authService.checkAuth();
+
+      set({ user });
+    } catch (error) {
+      errorHandler(error, "checkAuth");
+    } finally {
+      set({ checkingAuth: false });
+    }
+  },
+
+  login: async (loginInputs) => {
+    set({ loading: true });
+    try {
+      const user = await authService.login(loginInputs);
+
+      set({ user });
+      toast.success("Login successful");
+    } catch (error) {
+      errorHandler(error, "login", true);
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await authService.logout();
+
+      set({ user: null });
+      // TODO add resets
+      toast.success("Logout successful");
+    } catch (error) {
+      errorHandler(error, "logout", true);
+    }
+  },
+
+  refresh: async () => {
+    try {
+      await authService.refresh();
+    } catch (error) {
+      errorHandler(error, "refresh", true);
+    }
+  },
 }));
 
 export default useAuthStore;
