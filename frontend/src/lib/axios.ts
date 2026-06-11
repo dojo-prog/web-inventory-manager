6d05ceacd4ca5775ff0dd96360f04c0af85bfc97
dojo <djdumaluan.prog@gmail.com>
2026-06-11 15:58:53 +0800

    chore: create axios client

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios({
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "/api/v1"
+      : "http://localhost:3000/api/v1",
+  withCredentials: true,
+});
+
+export default axiosInstance;
