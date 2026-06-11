cba8e5d572b3e793b466d290c67e8f47e2184b6f
dojo <djdumaluan.prog@gmail.com>
2026-06-11 17:30:00 +0800

    feat: apply axios interceptor for refresh access token functionality

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import useAuthStore from "../features/auth/auth.store";
 
 const axiosInstance = axios.create({
   baseURL:
@@ -7,5 +8,25 @@ const axiosInstance = axios.create({
       : "http://localhost:3000/api/v1",
   withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await useAuthStore.getState().refresh();
+      } catch (error) {
+        useAuthStore.getState().logout();
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
