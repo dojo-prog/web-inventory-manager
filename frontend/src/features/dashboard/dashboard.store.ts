047d8a511461ab9aebe3aa4b6f9e93a0dbcfc8e4
dojo <djdumaluan.prog@gmail.com>
2026-06-12 04:57:56 +0800

    refactor: apply loading states to actions

--- frontend/src/features/dashboard/dashboard.store.ts
+++ frontend/src/features/dashboard/dashboard.store.ts
@@ -18,32 +18,42 @@ const useDashboardStore = create<DashboardState>((set) => ({
   fetchingRecentLogs: false,
 
   fetchSummary: async () => {
+    set({ fetchingSummary: true });
     try {
       const summary = await dashboardService.fetchSummary();
 
       set({ summary });
     } catch (error) {
       errorHandler(error, "fetchSummary", true);
+    } finally {
+      set({ fetchingSummary: false });
     }
   },
 
   fetchLowStocks: async () => {
+    set({ fetchingLowStockProducts: true });
     try {
       const lowStockProducts = await dashboardService.fetchLowStock();
 
       set({ lowStockProducts });
     } catch (error) {
       errorHandler(error, "fetchLowStocks", true);
+    } finally {
+      set({ fetchingLowStockProducts: false });
     }
   },
 
   fetchRecentLogs: async () => {
+    set({ fetchingRecentLogs: true });
+
     try {
       const recentActivityLogs = await dashboardService.fetchRecentLogs();
 
       set({ recentActivityLogs });
     } catch (error) {
       errorHandler(error, "fetchRecentLogs", true);
+    } finally {
+      set({ fetchingRecentLogs: false });
     }
   },
 }));
