df6b0f1d244295590c6f07dbc406111838d1f513
dojo <djdumaluan.prog@gmail.com>
2026-06-11 17:25:53 +0800

    feat: apply checkAuth on app render/re-render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
 import { Slide, ToastContainer } from "react-toastify";
 import AppRouter from "./app/routers";
+import useAuthStore from "./features/auth/auth.store";
+import { useEffect } from "react";
 
 const App = () => {
+  const { checkAuth, checkingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  // TODO add page loader
+  if (checkingAuth) return null;
+
   return (
     <>
       <AppRouter />
