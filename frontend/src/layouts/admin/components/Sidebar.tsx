f04de1d3da3c24ce3212a01c63c4725a4462e25c
dojo <djdumaluan.prog@gmail.com>
2026-06-11 17:27:27 +0800

    feat: apply logout functionality

--- frontend/src/layouts/admin/components/Sidebar.tsx
+++ frontend/src/layouts/admin/components/Sidebar.tsx
@@ -1,8 +1,11 @@
 import { LogOutIcon } from "lucide-react";
 import { adminRoutes } from "../../../app/routers/admin.routes";
 import SidebarTab from "./SidebarTab";
+import useAuthStore from "../../../features/auth/auth.store";
 
 const Sidebar = () => {
+  const { logout } = useAuthStore();
+
   return (
     <div className="h-screen w-full p-6 flex flex-col">
       {/* Header */}
@@ -22,7 +25,10 @@ const Sidebar = () => {
 
       {/* Footer */}
       <div className="h-16 border-t border-border flex flex-col item-center justify-center">
-        <button className="h-full w-full mt-4 px-4 flex items-center rounded-md text-secondary transition-colors duration-200 hover:bg-secondary/10 cursor-pointer">
+        <button
+          className="h-full w-full mt-4 px-4 flex items-center rounded-md text-secondary transition-colors duration-200 hover:bg-secondary/10 cursor-pointer"
+          onClick={logout}
+        >
           <LogOutIcon size={20} className="mr-2" />
           <span className="text-sm font-body">Logout</span>
         </button>
