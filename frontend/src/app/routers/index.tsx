54d390dd0e7fb87b342fa54b3ed270e6c24a3623
dojo <djdumaluan.prog@gmail.com>
2026-06-11 17:21:40 +0800

    chore: add auth route guard

--- frontend/src/app/routers/index.tsx
+++ frontend/src/app/routers/index.tsx
@@ -1,15 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { adminRoutes } from "./admin.routes";
 import AdminLayout from "../../layouts/admin/AdminLayout";
 import Signin from "../../pages/admin/Auth/Signin";
 import NotFound from "../../pages/NotFound";
+import useAuthStore from "../../features/auth/auth.store";
 
 const AppRouter = () => {
+  const { user } = useAuthStore();
+
   return (
     <BrowserRouter>
+      {/* TODO add permission guard */}
       <Routes>
         {/* Admin Main */}
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={user ? <AdminLayout /> : <Navigate to={"/admin/auth"} />}
+        >
           {adminRoutes.map((r, i) => (
             <Route
               key={r.path ?? `index-${i}`}
@@ -21,7 +28,10 @@ const AppRouter = () => {
         </Route>
 
         {/* Admin Auth */}
-        <Route path="/admin/auth" element={<Signin />}></Route>
+        <Route
+          path="/admin/auth"
+          element={!user ? <Signin /> : <Navigate to={"/admin"} />}
+        ></Route>
 
         {/* Not Found */}
         <Route path="*" element={<NotFound />} />
