68a55185058ea97ef148525a839be982ca6b1fff
dojo <djdumaluan.prog@gmail.com>
2026-06-12 09:10:21 +0800

    refactor: make searchbar dynamic

--- frontend/src/shared/Searchbar.tsx
+++ frontend/src/shared/Searchbar.tsx
@@ -1,15 +1,40 @@
 import { SearchIcon } from "lucide-react";
+import type { ChangeEvent } from "react";
 
-const Searchbar = () => {
+interface SearchbarProps {
+  placeholder: string;
+  id: string;
+  name: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  containerStyles?: string;
+  inputStyles?: string;
+}
+
+const Searchbar = ({
+  placeholder,
+  id,
+  name,
+  value,
+  onChange,
+  containerStyles,
+  inputStyles,
+}: SearchbarProps) => {
   return (
-    <div className="w-sm flex border border-border rounded h-10">
+    <div
+      className={`w-sm flex border border-border rounded h-10 ${containerStyles}`}
+    >
       <div className="h-full aspect-square border-r border-border flex items-center justify-center">
         <SearchIcon size={16} className="text-secondary" />
       </div>
       <input
         type="text"
-        placeholder="Enter brand ID or name"
-        className="flex-1 px-4 focus:outline-none text-sm font-label"
+        placeholder={placeholder}
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={`flex-1 px-4 focus:outline-none text-sm font-label ${inputStyles}`}
       />
     </div>
   );
