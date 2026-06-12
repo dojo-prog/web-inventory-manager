24b7e399a9fe6375375f86712be30f0677937f3e
dojo <djdumaluan.prog@gmail.com>
2026-06-12 15:52:34 +0800

    feat: apply initial brands hydration

--- frontend/src/pages/admin/Brands/Brands.tsx
+++ frontend/src/pages/admin/Brands/Brands.tsx
@@ -2,8 +2,16 @@ import Header from "./sections/Header";
 import BrandCard from "./components/BrandCard";
 import RegisterCard from "./components/RegisterCard";
 import BrandsTableContainer from "./components/BrandsTableContainer";
+import useBrandStore from "../../../features/brands/brand.store";
+import { useEffect } from "react";
 
 const Brands = () => {
+  const { fetchBrands, brands } = useBrandStore();
+
+  useEffect(() => {
+    fetchBrands({});
+  }, []);
+
   return (
     <div className="space-y-8">
       {/* Header */}
@@ -13,11 +21,9 @@ const Brands = () => {
 
       {/* Brand Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-        {/* Card */}
-        <BrandCard />
-        <BrandCard />
-        <BrandCard />
-        <BrandCard />
+        {brands.map((b) => (
+          <BrandCard key={b.id} item={b} />
+        ))}
 
         {/* Register */}
         <RegisterCard />
