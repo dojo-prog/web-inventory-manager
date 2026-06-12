4ede0ba04ddc962d066fe26e76a6204f1332505f
dojo <djdumaluan.prog@gmail.com>
2026-06-13 07:53:43 +0800

    chore: define mockBrands to be seeded

---
+++ backend/seeds/data/brand.data.ts
@@ -0,0 +1,32 @@
+export const mockBrands = [
+  {
+    name: "NIKE",
+    logo_url: "https://placehold.co/400x400/000000/ffffff?text=NIKE",
+    logo_path: "seeds/nike.png",
+  },
+  {
+    name: "ADIDAS",
+    logo_url: "https://placehold.co/400x400/22c55e/ffffff?text=ADIDAS",
+    logo_path: "seeds/adidas.png",
+  },
+  {
+    name: "PUMA",
+    logo_url: "https://placehold.co/400x400/ef4444/ffffff?text=PUMA",
+    logo_path: "seeds/puma.png",
+  },
+  {
+    name: "UNDER ARMOUR",
+    logo_url: "https://placehold.co/400x400/3b82f6/ffffff?text=UA",
+    logo_path: "seeds/under_armour.png",
+  },
+  {
+    name: "ASICS",
+    logo_url: null,
+    logo_path: null,
+  },
+  {
+    name: "NEW BALANCE",
+    logo_url: null,
+    logo_path: null,
+  },
+];
