43cc8b13411a5b00f1f82d5634dd627550b5f6a1
dojo <djdumaluan.prog@gmail.com>
2026-06-13 17:26:12 +0800

    chore: create category seed function

--- backend/seeds/main.seed.ts
+++ backend/seeds/main.seed.ts
@@ -1,5 +1,6 @@
 import { mockBrands } from "./data/brand.data";
 import db from "../src/database/db";
+import { mockCategories } from "./data/category.data";
 
 const seedBrands = async () => {
   console.log("🌱 Starting brand data seeding...");
@@ -28,4 +29,29 @@ const seedBrands = async () => {
   }
 };
 
-seedBrands();
+const seedCategories = async () => {
+  console.log("🌱 Starting categories data seeding...");
+
+  try {
+    await db.query("TRUNCATE TABLE categories CASCADE;");
+
+    for (const c of mockCategories) {
+      await db.query(
+        `
+        INSERT INTO categories (name, slug)
+        VALUES ($1, $2)
+        `,
+        [c.name, c.slug],
+      );
+      console.log(`🚀 Inserted category: ${c.name}`);
+    }
+
+    console.log("✅ Category seeding completed successfully!");
+  } catch (error) {
+    console.error("❌ Category seeding encountered an error:", error);
+  } finally {
+    if (typeof db.end === "function") {
+      await db.end();
+    }
+  }
+};
