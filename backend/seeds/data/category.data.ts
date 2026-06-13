5783c72fcb9db2af73911f41ffb9cabaa3736b4d
dojo <djdumaluan.prog@gmail.com>
2026-06-13 17:25:56 +0800

    chore: created mockCategories

---
+++ backend/seeds/data/category.data.ts
@@ -0,0 +1,34 @@
+export const mockCategories = [
+  {
+    name: "Road Running",
+    slug: "road-running",
+  },
+  {
+    name: "Trail & Outdoor",
+    slug: "trail-outdoor",
+  },
+  {
+    name: "Retro Sneakers",
+    slug: "retro-sneakers",
+  },
+  {
+    name: "Basketball",
+    slug: "basketball",
+  },
+  {
+    name: "Training & Gym",
+    slug: "training-gym",
+  },
+  {
+    name: "Slides & Recovery",
+    slug: "slides-recovery",
+  },
+  {
+    name: "Skate & Lifestyle",
+    slug: "skate-lifestyle",
+  },
+  {
+    name: "Track & Field Spikes",
+    slug: "track-field-spikes",
+  },
+];
