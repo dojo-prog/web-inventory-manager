26fee63ad772d24fbe8ecfc96e76409a9d0858bf
dojo <djdumaluan.prog@gmail.com>
2026-06-13 16:38:47 +0800

    chore: create header section ui

---
+++ frontend/src/pages/admin/Categories/sections/Header.tsx
@@ -0,0 +1,30 @@
+import { PlusIcon } from "lucide-react";
+import CustomButton from "../../../../shared/CustomButton";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between">
+      {/* Title */}
+      <div>
+        <h2 className="text-4xl font-headline font-semibold">
+          Categories Management
+        </h2>
+        <p className="text-xs text-secondary font-label mt-1">
+          Organize and manage footwear product classififcations.
+        </p>
+      </div>
+
+      {/* Actions */}
+      <div>
+        <CustomButton
+          title="Add Categories"
+          Icon={PlusIcon}
+          buttonStyles="h-12 rounded-sm px-6 bg-primary hover:bg-primary-hover"
+          titleStyles="text-white text-sm font-body"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
