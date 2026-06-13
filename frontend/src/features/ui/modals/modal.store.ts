ca986428f4e0120528c46ff2c78725cea7253454
dojo <djdumaluan.prog@gmail.com>
2026-06-13 08:10:24 +0800

    feat: create modal store with brand related states & actions

---
+++ frontend/src/features/ui/modals/modal.store.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import type { ModalState } from "./modal.types";
+
+const useModalStore = create<ModalState>((set) => ({
+  // Brands
+  brandModalOpen: false,
+  modalType: null,
+  selectedBrand: null,
+  openBrandModal: (type, brand) => {
+    if (brand) {
+      set({ selectedBrand: brand });
+    }
+
+    set({ modalType: type, brandModalOpen: true });
+  },
+  closeBrandModal: () => {
+    set({ selectedBrand: null, modalType: null, brandModalOpen: false });
+  },
+}));
+
+export default useModalStore;
---
+++ frontend/src/features/ui/modals/modal.types.ts
@@ -0,0 +1,12 @@
+import type { DetailedBrand } from "@web-inventory-manager/shared";
+
+type ModalType = "create" | "update" | null;
+
+export interface ModalState {
+  // Brand Modal
+  brandModalOpen: boolean;
+  modalType: ModalType;
+  selectedBrand: DetailedBrand | null;
+  openBrandModal: (type: ModalType, brand?: DetailedBrand) => void;
+  closeBrandModal: () => void;
+}
