e0776a8e15d1b880a7eeb3da6e3a87dfb9ed0618
dojo <djdumaluan.prog@gmail.com>
2026-06-13 20:42:37 +0800

    feat: apply search & pagination functionality

--- frontend/src/pages/admin/Categories/components/TableHeader.tsx
+++ frontend/src/pages/admin/Categories/components/TableHeader.tsx
@@ -1,7 +1,38 @@
+import useCategoryStore from "../../../../features/categories/category.store";
+import useDebounce from "../../../../hooks/useDebounce";
+import { useEffect } from "react";
+import getPaginationRange from "../../../../utils/getPaginationRange";
 import Searchbar from "../../../../shared/Searchbar";
 import PageControl from "../../../../shared/PageControl";
 
 const TableHeader = () => {
+  const { total_count, filters, setFilters, fetchCategories } =
+    useCategoryStore();
+  const { q, page, limit } = filters;
+
+  const handleSearchChange = (e: any) => {
+    setFilters({ q: e.target.value });
+  };
+
+  const debouncedQ = useDebounce(q, 400);
+
+  useEffect(() => {
+    setFilters({ page: 1 });
+    fetchCategories({ q: debouncedQ });
+  }, [debouncedQ]);
+
+  const { from, to } = getPaginationRange(
+    total_count,
+    page as number,
+    limit as number,
+  );
+
+  const handlePageChange = async (newPage: number) => {
+    setFilters({ page: newPage });
+
+    await fetchCategories(filters);
+  };
+
   return (
     <div className="border-b border-border p-5 shrink-0 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 bg-surface">
       {/* Title block */}
@@ -12,8 +43,12 @@ const TableHeader = () => {
 
         {/* Row Indicator */}
         <p className="text-xs font-medium font-body text-text-muted">
-          Showing <span className="font-semibold text-text">1-20</span> of{" "}
-          <span className="font-semibold text-text">142</span> entries
+          Showing{" "}
+          <span className="font-semibold text-text">
+            {from}-{to}
+          </span>{" "}
+          of <span className="font-semibold text-text">{total_count}</span>{" "}
+          entries
         </p>
       </div>
 
@@ -23,11 +58,16 @@ const TableHeader = () => {
           placeholder="Enter brand ID or name"
           id="q"
           name="q"
-          value=""
-          onChange={() => {}}
+          value={q || ""}
+          onChange={handleSearchChange}
         />
 
-        <PageControl />
+        <PageControl
+          page={page as number}
+          limit={limit as number}
+          totalCount={total_count}
+          onPageChange={handlePageChange}
+        />
       </div>
     </div>
   );
