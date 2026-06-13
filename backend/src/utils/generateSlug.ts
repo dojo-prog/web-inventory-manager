640f4f93a141bac1e8698f18061d39a6ab88aa24
dojo <djdumaluan.prog@gmail.com>
2026-06-13 21:29:16 +0800

    chore: create util that generates slug

---
+++ backend/src/utils/generateSlug.ts
@@ -0,0 +1,10 @@
+const generateSlug = (value: string): string => {
+  return value
+    .trim()
+    .toLowerCase()
+    .replace(/['"]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
+
+export default generateSlug;
