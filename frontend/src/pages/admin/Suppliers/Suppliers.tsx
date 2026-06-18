f1c1cebe495bae6d404a49ebf1bd9581eced5071
dojo <djdumaluan.prog@gmail.com>
2026-06-18 14:55:09 +0800

    feat: apply initial suppliers hydration

--- frontend/src/pages/admin/Suppliers/Suppliers.tsx
+++ frontend/src/pages/admin/Suppliers/Suppliers.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+import useSupplierStore from "../../../features/suppliers/supplier.store";
 import SupplierDetail from "./components/SupplierDetail";
 import SuppliersContainerTable from "./components/SuppliersTableContainer";
 import Header from "./sections/Header";
 
 const Suppliers = () => {
+  const { fetchSuppliers, filters } = useSupplierStore();
+
+  useEffect(() => {
+    fetchSuppliers(filters);
+  }, []);
+
   return (
     <div className="space-y-8">
       {/* Header */}
