4c4a2e05f621a78bb2fcf3e280c95fc4edfbdbb0
dojo <djdumaluan.prog@gmail.com>
2026-06-18 15:32:04 +0800

    feat: apply query and pagination functionality

--- frontend/src/pages/admin/Suppliers/components/TableHeader.tsx
+++ frontend/src/pages/admin/Suppliers/components/TableHeader.tsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
+import useSupplierStore from "../../../../features/suppliers/supplier.store";
+import useDebounce from "../../../../hooks/useDebounce";
 import PageControl from "../../../../shared/PageControl";
 import Searchbar from "../../../../shared/Searchbar";
+import getPaginationRange from "../../../../utils/getPaginationRange";
 
 const TableHeader = () => {
+  const { filters, setFilters, fetchSuppliers, total_count } =
+    useSupplierStore();
+  const { q, page, limit } = filters;
+
+  const handleSearchChange = (e: any) => {
+    setFilters({ q: e.target.value });
+  };
+
+  const debouncedQ = useDebounce(q, 400);
+
+  useEffect(() => {
+    setFilters({ page: 1 });
+    fetchSuppliers({ q: debouncedQ });
+  }, [debouncedQ]);
+
+  const { from, to } = getPaginationRange(
+    total_count,
+    page as number,
+    limit as number,
+  );
+
+  const handlePageChange = async (newPage: number) => {
+    setFilters({ page: newPage });
+
+    await fetchSuppliers(filters);
+  };
+
   return (
     <div className="border-b border-border p-5 shrink-0 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 bg-surface">
       {/* Title block */}
@@ -12,16 +43,31 @@ const TableHeader = () => {
 
         {/* Row Indicator */}
         <p className="text-xs font-medium font-body text-text-muted">
-          Showing <span className="font-semibold text-text">1-10</span> of{" "}
-          <span className="font-semibold text-text">100</span> entries
+          Showing{" "}
+          <span className="font-semibold text-text">
+            {from}-{to}
+          </span>{" "}
+          of <span className="font-semibold text-text">{total_count}</span>{" "}
+          entries
         </p>
       </div>
 
       {/* Control Actions Panel */}
       <div className="flex flex-wrap items-center gap-4">
-        <Searchbar placeholder="Enter supplier ID or name" id="q" name="q" />
+        <Searchbar
+          placeholder="Enter ID, name, contact name, email or website "
+          id="q"
+          name="q"
+          value={q || ""}
+          onChange={handleSearchChange}
+        />
 
-        <PageControl />
+        <PageControl
+          page={page as number}
+          limit={limit as number}
+          totalCount={5}
+          onPageChange={handlePageChange}
+        />
       </div>
     </div>
   );
