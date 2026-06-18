59e801c4e54d25846930b845712c28030e419e02
dojo <djdumaluan.prog@gmail.com>
2026-06-18 13:23:51 +0800

    chore: scaffold Suppliers page layout

--- frontend/src/pages/admin/Suppliers/Suppliers.tsx
+++ frontend/src/pages/admin/Suppliers/Suppliers.tsx
@@ -1,5 +1,22 @@
+import SupplierDetail from "./components/SupplierDetail";
+import SuppliersTable from "./components/SuppliersTable";
+import Header from "./sections/Header";
+
 const Suppliers = () => {
-  return <div>Suppliers</div>;
+  return (
+    <div className="space-y-8">
+      {/* Header */}
+      <div>
+        <Header />
+      </div>
+
+      {/* Table & Detail */}
+      <div className="flex space-x-4">
+        <SuppliersTable />
+        <SupplierDetail />
+      </div>
+    </div>
+  );
 };
 
 export default Suppliers;
---
+++ frontend/src/pages/admin/Suppliers/components/SuppliersTable.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuppliersTable = () => {
+  return <div>SuppliersTable</div>;
+};
+
+export default SuppliersTable;
