d0bf60ed9de7234d044f8c9424683c5c09dc9f21
dojo <djdumaluan.prog@gmail.com>
2026-06-18 14:09:36 +0800

    feat: implement supplier feature

--- frontend/src/features/suppliers/supplier.service.ts
+++ frontend/src/features/suppliers/supplier.service.ts
@@ -4,14 +4,29 @@ import type {
   SupplierFilters,
   UpdateSupplierInput,
 } from "@web-inventory-manager/shared";
+import axios from "../../lib/axios";
 
 export const fetchSuppliers = async (
   filters: SupplierFilters,
-): Promise<Supplier[]> => {};
+): Promise<Supplier[]> => {
+  const res = await axios.get("/suppliers", { params: filters });
+
+  return res.data.suppliers;
+};
+
 export const addSupplier = async (
   inputs: AddSupplierInput,
-): Promise<Supplier> => {};
+): Promise<Supplier> => {
+  const res = await axios.post("/suppliers", inputs);
+
+  return res.data.newSupplier;
+};
+
 export const updateSupplier = async (
   supplierId: string,
   inputs: UpdateSupplierInput,
-): Promise<Supplier> => {};
+): Promise<Supplier> => {
+  const res = await axios.put(`/suppliers/${supplierId}`, inputs);
+
+  return res.data.updatedSupplier;
+};
--- frontend/src/features/suppliers/supplier.store.ts
+++ frontend/src/features/suppliers/supplier.store.ts
@@ -1,5 +1,8 @@
 import { create } from "zustand";
 import type { SupplierState } from "./supplier.types";
+import * as supplierService from "./supplier.service";
+import errorHandler from "../../utils/errorHandler";
+import { toast } from "react-toastify";
 
 const useSupplierStore = create<SupplierState>((set) => ({
   suppliers: [],
@@ -14,11 +17,60 @@ const useSupplierStore = create<SupplierState>((set) => ({
     limit: 20,
   },
 
-  setFilters: (newFilters) => {},
+  setFilters: (newFilters) =>
+    set((state) => ({
+      filters: { ...state.filters, ...newFilters },
+    })),
 
-  fetchSuppliers: async () => {},
-  addSupplier: async (inputs) => {},
-  updateSupplier: async (supplierId, inputs) => {},
+  fetchSuppliers: async (filters) => {
+    set({ fetchingSuppliers: true });
+    try {
+      const suppliers = await supplierService.fetchSuppliers(filters);
+
+      set({ suppliers });
+    } catch (error) {
+      errorHandler(error, "fetchSuppliers", true);
+    } finally {
+      set({ fetchingSuppliers: false });
+    }
+  },
+
+  addSupplier: async (inputs) => {
+    set({ loading: true });
+    try {
+      const newSupplier = await supplierService.addSupplier(inputs);
+
+      set((state) => ({
+        suppliers: [newSupplier, ...state.suppliers],
+      }));
+      toast.success("New supplier added");
+    } catch (error) {
+      errorHandler(error, "addSupplier", true);
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  updateSupplier: async (supplierId, inputs) => {
+    set({ loading: false });
+    try {
+      const updatedSupplier = await supplierService.updateSupplier(
+        supplierId,
+        inputs,
+      );
+
+      set((state) => ({
+        suppliers: state.suppliers.map((s) =>
+          s.id === supplierId ? updatedSupplier : s,
+        ),
+      }));
+      toast.success("Supplier updated");
+    } catch (error) {
+      errorHandler(error, "updateSupplier", true);
+    } finally {
+      set({ loading: false });
+    }
+  },
 }));
 
 export default useSupplierStore;
