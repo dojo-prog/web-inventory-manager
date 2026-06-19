168e23b86a9bd4a94c3ba9d209890145b8c15b4d
dojo <djdumaluan.prog@gmail.com>
2026-06-19 19:23:17 +0800

    feat: apply inital products hydration on page load

--- frontend/src/pages/admin/Products/Products.tsx
+++ frontend/src/pages/admin/Products/Products.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import useProductStore from "../../../features/products/product.store";
 import ProductsTableContainer from "./components/ProductsTableContainer";
 import Header from "./sections/Header";
 
 const Products = () => {
+  const { fetchProducts, filters } = useProductStore();
+
+  useEffect(() => {
+    if (filters) {
+      fetchProducts(filters);
+    }
+  }, []);
+
   return (
     <div className="space-y-8">
       {/* Header */}
