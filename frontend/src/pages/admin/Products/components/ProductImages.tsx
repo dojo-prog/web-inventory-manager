3647eb698c03e957b6112219f26a4a5d45fff5ad
dojo <djdumaluan.prog@gmail.com>
2026-06-20 14:56:07 +0800

    feat: add image preview

--- frontend/src/pages/admin/Products/components/ProductImages.tsx
+++ frontend/src/pages/admin/Products/components/ProductImages.tsx
@@ -1,7 +1,8 @@
-import { useEffect, type ChangeEvent } from "react";
+import { useEffect, useState, type ChangeEvent } from "react";
 import useProductStore from "../../../../features/products/product.store";
 import AddImageButton from "./AddImageButton";
 import useProductImageStore from "../../../../features/product_images/product_image.store";
+import type { ProductImage } from "@web-inventory-manager/shared";
 
 const ProductImages = () => {
   const { selectedProduct } = useProductStore();
@@ -30,23 +31,41 @@ const ProductImages = () => {
     }
   };
 
+  const [selectedImage, setSelectedImage] = useState<ProductImage | undefined>(
+    product_images[0],
+  );
+
   return (
     <div className="w-full">
       {/* Preview Image */}
-      <div className="h-90 border bg-gray-200/10 border-border rounded-md shadow mb-4"></div>
+      <div className="h-90 border bg-gray-200/10 border-border rounded-md shadow mb-4">
+        <img
+          src={selectedImage?.image_url}
+          alt="image_preview"
+          className="h-full w-full object-contain"
+        />
+      </div>
 
       {/* Images */}
       <div className="grid grid-cols-6 gap-4">
         {product_images.length > 0 &&
-          product_images.map((pi) => (
-            <div key={pi.id} className="w-full aspect-square bg-gray-400/10">
-              <img
-                src={pi.image_url}
-                alt="product_image"
-                className="h-full w-full object-cover"
-              />
-            </div>
-          ))}
+          product_images.map((pi) => {
+            const is_selected = selectedImage?.id === pi.id;
+
+            return (
+              <div
+                key={pi.id}
+                className={`w-full aspect-square ${is_selected ? "bg-primary/10 border border-primary" : "bg-gray-400/10"} rounded-md cursor-pointer`}
+                onClick={() => setSelectedImage(pi)}
+              >
+                <img
+                  src={pi.image_url}
+                  alt="product_image"
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            );
+          })}
 
         {/* Add Image Button */}
         <AddImageButton id="image" name="image" fileChange={handleFileChange} />
