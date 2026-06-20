de7e94f8942ad1fa1aadeee814d77f2692be9b15
dojo <djdumaluan.prog@gmail.com>
2026-06-20 14:50:30 +0800

    feat: apply add product image functionality

--- frontend/src/pages/admin/Products/components/ProductImages.tsx
+++ frontend/src/pages/admin/Products/components/ProductImages.tsx
@@ -1,17 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, type ChangeEvent } from "react";
 import useProductStore from "../../../../features/products/product.store";
 import AddImageButton from "./AddImageButton";
 import useProductImageStore from "../../../../features/product_images/product_image.store";
 
 const ProductImages = () => {
   const { selectedProduct } = useProductStore();
-  const { fetchProductImages, product_images } = useProductImageStore();
+  const { fetchProductImages, product_images, addProductImage } =
+    useProductImageStore();
 
   useEffect(() => {
     if (!selectedProduct?.id) return;
 
     fetchProductImages(selectedProduct.id);
   }, [selectedProduct?.id]);
+
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!selectedProduct?.id) return;
+
+    const files = e.target.files;
+
+    if (!files || files.length === 0) return;
+
+    const image = files[0];
+
+    try {
+      await addProductImage(selectedProduct.id, { image });
+    } finally {
+      e.target.value = "";
+    }
+  };
 
   return (
     <div className="w-full">
@@ -32,7 +49,7 @@ const ProductImages = () => {
           ))}
 
         {/* Add Image Button */}
-        <AddImageButton />
+        <AddImageButton id="image" name="image" fileChange={handleFileChange} />
       </div>
     </div>
   );
