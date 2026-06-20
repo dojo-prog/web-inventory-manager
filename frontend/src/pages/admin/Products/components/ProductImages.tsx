83e4cac24785358e5e3b537c2d382efca456d70c
dojo <djdumaluan.prog@gmail.com>
2026-06-20 14:26:54 +0800

    feat: apply product image fetching + images rendering

--- frontend/src/pages/admin/Products/components/ProductImages.tsx
+++ frontend/src/pages/admin/Products/components/ProductImages.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import useProductStore from "../../../../features/products/product.store";
 import AddImageButton from "./AddImageButton";
+import useProductImageStore from "../../../../features/product_images/product_image.store";
 
 const ProductImages = () => {
+  const { selectedProduct } = useProductStore();
+  const { fetchProductImages, product_images } = useProductImageStore();
+
+  useEffect(() => {
+    if (!selectedProduct?.id) return;
+
+    fetchProductImages(selectedProduct.id);
+  }, [selectedProduct?.id]);
+
   return (
     <div className="w-full">
       {/* Preview Image */}
@@ -8,7 +20,16 @@ const ProductImages = () => {
 
       {/* Images */}
       <div className="grid grid-cols-6 gap-4">
-        <div className="w-full aspect-square bg-gray-400/10"></div>
+        {product_images.length > 0 &&
+          product_images.map((pi) => (
+            <div key={pi.id} className="w-full aspect-square bg-gray-400/10">
+              <img
+                src={pi.image_url}
+                alt="product_image"
+                className="h-full w-full object-cover"
+              />
+            </div>
+          ))}
 
         {/* Add Image Button */}
         <AddImageButton />
