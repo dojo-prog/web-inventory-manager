afcf1f98f59390e1cea46cfe71ae471aee224070
dojo <djdumaluan.prog@gmail.com>
2026-06-20 14:50:19 +0800

    feat: add input file selection functionality

--- frontend/src/pages/admin/Products/components/AddImageButton.tsx
+++ frontend/src/pages/admin/Products/components/AddImageButton.tsx
@@ -1,11 +1,32 @@
 import { CameraIcon } from "lucide-react";
+import type { ChangeEvent } from "react";
 
-const AddImageButton = () => {
+type Props = {
+  id: string;
+  name: string;
+  fileChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const AddImageButton = ({ id, name, fileChange }: Props) => {
   return (
-    <div className="w-full aspect-square bg-gray-400/10 flex flex-col items-center justify-center border-2 border-dashed border-gray-300 cursor-pointer">
-      <CameraIcon size={15} className="text-secondary" />
-      <span className="text-[10px] font-label text-secondary">Add Image</span>
-    </div>
+    <label
+      htmlFor={id}
+      className="w-full aspect-square bg-gray-400/10 flex flex-col items-center justify-center border-2 border-dashed border-gray-300 cursor-pointer hover:bg-gray-400/20 transition-colors"
+    >
+      <input
+        type="file"
+        id={id}
+        name={name}
+        accept="image/*"
+        className="hidden"
+        onChange={fileChange}
+      />
+
+      <CameraIcon size={15} className="text-secondary mb-1" />
+      <span className="text-[10px] font-label text-secondary select-none">
+        Add Image
+      </span>
+    </label>
   );
 };
 
