4feadebb03cb96b5efd1021f249cad317b4aa41d
dojo <djdumaluan.prog@gmail.com>
2026-06-21 10:51:49 +0800

    feat: register product modal  with rendering condition

--- frontend/src/pages/admin/Products/Products.tsx
+++ frontend/src/pages/admin/Products/Products.tsx
@@ -2,9 +2,12 @@ import { useEffect } from "react";
 import useProductStore from "../../../features/products/product.store";
 import ProductsTableContainer from "./components/ProductsTableContainer";
 import Header from "./sections/Header";
+import useModalStore from "../../../features/ui/modals/modal.store";
+import ProductModal from "./components/ProductModal";
 
 const Products = () => {
   const { fetchProducts, filters } = useProductStore();
+  const { productModalOpen } = useModalStore();
 
   useEffect(() => {
     if (filters) {
@@ -23,6 +26,8 @@ const Products = () => {
       <div className="h-[calc(100vh-16rem)] flex space-x-4">
         <ProductsTableContainer />
       </div>
+
+      {productModalOpen && <ProductModal />}
     </div>
   );
 };
