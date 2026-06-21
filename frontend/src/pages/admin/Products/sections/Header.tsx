f6fa4ff8aa92f72dba35ad65865b6660d6a224ec
dojo <djdumaluan.prog@gmail.com>
2026-06-21 11:10:04 +0800

    feat: add add product button to open product modal

--- frontend/src/pages/admin/Products/sections/Header.tsx
+++ frontend/src/pages/admin/Products/sections/Header.tsx
@@ -1,6 +1,11 @@
+import { PlusIcon } from "lucide-react";
+import CustomButton from "../../../../shared/CustomButton";
+import useModalStore from "../../../../features/ui/modals/modal.store";
+
 const Header = () => {
+  const { openProductModal } = useModalStore();
   return (
-    <div>
+    <div className="flex items-center justify-between">
       <div>
         <h2 className="text-4xl font-headline font-semibold">
           Product Management
@@ -8,6 +13,16 @@ const Header = () => {
         <p className="text-xs text-secondary font-label mt-1">
           Manage your enterprise inventory and stock levels.
         </p>
+      </div>
+
+      <div>
+        <CustomButton
+          title="Add New Product"
+          Icon={PlusIcon}
+          buttonStyles="h-12 rounded-sm px-6 bg-primary hover:bg-primary-hover"
+          titleStyles="text-white text-sm font-body"
+          onClick={() => openProductModal("create")}
+        />
       </div>
     </div>
   );
