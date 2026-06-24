cfee0be8b319549192449db602d3ffcde1770cde
dojo <djdumaluan.prog@gmail.com>
2026-06-24 15:46:27 +0800

    refactor: alter product_variants db schema to have color_name and color_hex

--- backend/src/database/schemas/007_product_variants.sql
+++ backend/src/database/schemas/007_product_variants.sql
@@ -4,10 +4,11 @@ CREATE TABLE product_variants (
   supplier_id uuid REFERENCES public.suppliers(id) NOT NULL, 
 
   size text NOT NULL, 
-  color text NOT NULL, 
+  color_name text NOT NULL,
+  color_hex text NOT NULL, 
   stock_quantity int NOT NULL DEFAULT 0,
 
   created_at timestamptz NOT NULL DEFAULT now(),
 
-  CONSTRAINT unique_product_variant UNIQUE (product_id, size, color)
+  CONSTRAINT unique_product_variant UNIQUE (product_id, size, color_name, color_hex)
 );
