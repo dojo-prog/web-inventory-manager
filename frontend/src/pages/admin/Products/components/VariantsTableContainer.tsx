04f73fc357d80fb250726370b7092c146001f3fa
dojo <djdumaluan.prog@gmail.com>
2026-06-25 13:51:07 +0800

    feat: apply variants fetch

--- frontend/src/pages/admin/Products/components/VariantsTableContainer.tsx
+++ frontend/src/pages/admin/Products/components/VariantsTableContainer.tsx
@@ -2,9 +2,20 @@ import { PlusIcon } from "lucide-react";
 import CustomButton from "../../../../shared/CustomButton";
 import VariantsTable from "./VariantsTable";
 import useModalStore from "../../../../features/ui/modals/modal.store";
+import useProductVariantStore from "../../../../features/product_variants/product_variant.store";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 const VariantsTableContainer = () => {
+  const { fetchVariants } = useProductVariantStore();
   const { openProductVariantModal } = useModalStore();
+
+  const { productId } = useParams<{ productId: string }>();
+
+  useEffect(() => {
+    if (!productId) return;
+    fetchVariants(productId as string);
+  }, [productId]);
 
   return (
     <div className="h-full flex flex-col overflow-hidden rounded-md border border-border bg-surface shadow-sm">
