a923cc9b620dc3f526d1d710ae792c0fc2bcf54b
dojo <djdumaluan.prog@gmail.com>
2026-06-25 16:25:20 +0800

    chore: apply guard

--- frontend/src/schemas/user.ts
+++ frontend/src/schemas/user.ts
@@ -4,13 +4,21 @@ import {
 } from "@web-inventory-manager/shared";
 import { z } from "zod";
 
-export const AddUserFormSchema = AddUserInputShape.extend({
-  avatar: z.instanceof(File, { message: "Avatar uploaded is not a file" }),
-});
+export const AddUserFormSchema = AddUserInputShape
+  ? AddUserInputShape.extend({
+      avatar: z
+        .instanceof(File, { message: "Avatar uploaded is not a file" })
+        .optional(),
+    })
+  : z.object({});
 
-export const UpdateUserFormSchema = UpdateUserInputShape.extend({
-  avatar: z.instanceof(File, { message: "Avatar uploaded is not a file" }),
-});
+export const UpdateUserFormSchema = UpdateUserInputShape
+  ? UpdateUserInputShape.extend({
+      avatar: z
+        .instanceof(File, { message: "Avatar uploaded is not a file" })
+        .optional(),
+    })
+  : z.object({});
 
 export type AddUserForm = z.input<typeof AddUserFormSchema>;
 export type UpdateUserForm = z.input<typeof UpdateUserFormSchema>;
